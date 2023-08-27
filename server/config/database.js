2e638f037d378645c3d3913168dd12548cc2d5c8
gshalom910 <91600692+gshalom910@users.noreply.github.com>
2023-08-27 20:32:18 +0300

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 const mysql = require("mysql2");
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.MYSQL_DB,
-  waitForConnections: true,
-  connectionLimit: 10,
-  password: process.env.DB_PASS,
-});
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   database: process.env.MYSQL_DB,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   password: process.env.DB_PASS,
+// });
 
-// const pool = mysql.createPool(process.env.DATABASE_URL);
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 pool.getConnection(function (err, conn) {
   if (err) {
